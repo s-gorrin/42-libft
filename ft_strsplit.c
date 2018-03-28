4343dcacd1faba8cd5cc5b95054928b262eaca85
Seth Gorrin <sgorrin@gmail.com>
2018-03-27 17:00:51 -0700

    Turns gh-libft into functional libft folder.

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,19 +6,16 @@
 /*   By: sgorrin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/12 12:44:00 by sgorrin           #+#    #+#             */
-/*   Updated: 2018/03/12 18:08:04 by snake            ###   ########.fr       */
+/*   Updated: 2018/03/14 15:06:58 by sgorrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
-#include <string.h>
-#include <stdlib.h>
+#include "libft.h"
 
-#include <stdio.h>
-static int	wc(char const *s, char c)
+static int		wc(char const *s, char c)
 {
 	size_t	i;
-	int	count;
+	int		count;
 
 	i = 0;
 	count = 0;
@@ -46,28 +43,11 @@ static size_t	len(char const *s, char c, size_t i)
 	return (l);
 }
 
-/*static char	*dup(char const *s, size_t i, size_t len)
-{
-	char	*sub;
-	size_t	j;
-
-	j = 0;
-	sub = (char *)malloc(sizeof(char) * len + 1);
-	while (i < len)
-	{
-		sub[j] = s[i];
-		i++;
-		j++;
-	}
-	sub[j] = '\0';
-	return (sub);
-}*/
-
-char		**ft_strsplit(char const *s, char c)
+char			**ft_strsplit(char const *s, char c)
 {
 	char	**ret;
-	int	words;
-	int	tmp;
+	int		words;
+	int		tmp;
 	size_t	i;
 	size_t	j;
 
@@ -76,41 +56,17 @@ char		**ft_strsplit(char const *s, char c)
 	if (!s)
 		return (NULL);
 	words = wc(s, c);
-	ret = (char **)malloc(sizeof(char *) * (words + 1));
+	if (!(ret = (char **)malloc(sizeof(char *) * (words + 1))))
+		return (NULL);
 	while (s[i])
 	{
 		while (s[i] == c)
 			i++;
 		tmp = len(s, c, i);
 		if (s[i])
-			ret[j++] = strndup(s + i, tmp);
-//		while (s[i] != c && s[i])
-//			i++;
+			ret[j++] = ft_strndup(s + i, tmp);
 		i += tmp;
-		printf("%p\n", ret[j - 1]);
 	}
-	printf("%p\n", ret[j]);
 	ret[j] = 0;
 	return (ret);
-}
-
-int		main(/*int ac, char **av*/)
-{
-	char	**split;
-	char	str[] = "*split*me*up*";
-	char	c = '*';
-	int	i;
-
-	i = 0;
-//	str = strdup(av[1]);
-//	c = av[2][0];
-//	if (ac != 3)
-//		printf("%d\n", ac);
-	split = ft_strsplit((char const *)str, c);
-	while (split[i])
-	{
-		printf("%s\n", split[i]);
-		i++;
-	}
-	return (0);
 }
